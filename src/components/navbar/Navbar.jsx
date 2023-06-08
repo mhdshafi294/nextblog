63696cdff11dd319e1c22aebe46beac70a069039
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-06-08 19:59:20 +0300

    darkLightModeToggle

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,12 +4,14 @@ import React from 'react'
 import Link from 'next/link'
 import styles from "./navbar.module.css";
 import { links } from '@/constants/constants'
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>Lamamia</Link>
       <div className={styles.links}>
+        <DarkModeToggle />
         {links.map(link => (
           <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
